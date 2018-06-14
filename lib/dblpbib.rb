e290e4f110bf8658a08a2dfa6c6b3df2e334acf8
PJK <me@pavelkalvoda.com>
2018-06-14 21:50:22 +0200

    Create a simple test suite

--- lib/dblpbib.rb
+++ lib/dblpbib.rb
@@ -1,5 +1,15 @@
 require "dblpbib/version"
 
 module Dblpbib
-  # Your code goes here...
+	def self.scan_keys(document)
+		document
+			.gsub(/(?<!\\)%.*$/, '')
+			.scan(/\\cite(\[[^\]]+\])?\{([^\}]+)\}/)
+			.map(&:pop)
+			.flat_map { |keyset| keyset.split(',') }
+			.map(&:strip)
+			.keep_if { |key| key.start_with?('DBLP:') }
+			.map { |key| key.sub('DBLP:', '') }
+			.uniq
+	end
 end
